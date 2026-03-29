def2739b26de34c45af896f55fdf1941868744c1
Pradeep2027 <shabadpradeep1@gmail.com>
2026-03-29 12:06:50 +0530

    About component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-// import About from './Components/About';
-import Textutils from './Components/Textutils';
+import About from './Components/About';
+// import Textutils from './Components/Textutils';
 import React, { useState } from 'react';
 
 function App() {
 
   const [mode, setMode] = useState('light');
+
+  const [style, setStyle] = useState({
+    color: 'black',
+    backgroundColor: 'white'
+  });
   
   const toggleMode = () => {
     console.log("ToggleMode trigered");
     if (mode === 'light')  {
       setMode('dark');
+      setStyle({
+        color: 'white',
+        backgroundColor: 'black'
+      });
       document.body.style.backgroundColor = 'grey';
     }
     else  {
       setMode('light');
+      setStyle({
+        color: 'black',
+        backgroundColor: 'white'
+      });
       document.body.style.backgroundColor = 'white';
     }
   }
@@ -26,8 +39,8 @@ function App() {
     <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className="container my-3">
-      {/* <About/> */}
-      <Textutils heading="Enter the text to analyze below:" mode={mode}> </Textutils>
+      <About mode={mode} style={style}> </About>
+      {/* <Textutils heading="Enter the text to analyze below:" mode={mode}> </Textutils> */}
     </div>
     </>
   );

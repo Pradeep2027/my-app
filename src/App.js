4d625cba9f92e833bd99df7653ef7f17bace838d
Pradeep2027 <shabadpradeep1@gmail.com>
2026-03-29 10:24:12 +0530

    Created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+// import About from './Components/About';
 import Textutils from './Components/Textutils';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState('dark');
+  
+  const toggleMode = () => {
+    console.log("ToggleMode trigered");
+    if (mode === 'light')  {
+      setMode('dark');
+    }
+    else  {
+      setMode('light');
+    }
+  }
+
+  // const [style,setStyle] = useState
+
   return (
     <>
-    <Navbar title="Text Utils">
-
-    </Navbar>
-    <Textutils heading="Home"> </Textutils>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <div className="container my-3">
+      {/* <About/> */}
+      <Textutils heading="Enter the text to analyze below:"> </Textutils>
+    </div>
     </>
   );
 }
